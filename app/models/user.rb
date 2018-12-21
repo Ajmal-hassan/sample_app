2c50953a084cbce7ef231500a67b2514c4f1dcf1
Ajmal hassan <ajmalhassan651@gmail.com>
2018-12-21 10:20:43 +0500

    Work on validation

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  # attr_accessor :name, :email
+  validates :name, presence: true, length: {maximum: 50}
+  validates :email, presence: true
+      def new
+
+  end
+end
